0b5a5674787a4b8bbf2b43e718c08a6832d4d8e8
Kodjo Adanledji <kodjo.adanledji@gmail.com>
2024-05-31 12:45:48 +0200

    producers set

--- producers/body_temperature_producer.py
+++ producers/body_temperature_producer.py
@@ -1,17 +1,54 @@
 import csv
 import time
-from kafka import KafkaProducer
-import json
+import socket
+from confluent_kafka import Producer
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+conf = {"bootstrap.servers": "kafka:9092", "client.id": socket.gethostname()}
+producer = Producer(conf)
+
+
+def delivery_report(err, msg):
+    """Called once for each message produced to indicate delivery result.
+    Triggered by poll() or flush()."""
+    if err is not None:
+        print("Message delivery failed: {}".format(err))
+    else:
+        print("Message delivered to {} [{}]".format(msg.topic(), msg.partition()))
+
+
+def read_first_line_csv(filename):
+    try:
+        with open(filename, mode="r", newline="") as file:
+            reader = csv.reader(file)
+            first_line = next(reader)
+            return first_line
+    except FileNotFoundError:
+        print("The file does not exist.")
+    except StopIteration:
+        print("The file is empty.")
+
+
 
 def read_csv_and_send_to_kafka(csv_file):
-    with open(csv_file, mode='r') as file:
+    with open(csv_file, mode="r") as file:
+        headers = set(read_first_line_csv(csv_file))
+        headers = [header.strip() for header in headers]
+        header_set = set(headers)
+
         reader = csv.DictReader(file)
         for row in reader:
-            producer.send('health_data', value=row)
+            
+            producer.produce(
+                "body_temperature",
+                key=str(header_set),
+                value=str(list(row.values())),
+                callback=delivery_report,
+            )
+            producer.poll(1)
             time.sleep(1)  # simulate real-time data streaming
 
+
 if __name__ == "__main__":
-    csv_file = 'path_to_your_csv_file.csv'
+    csv_file = "data/ACC.csv"
     read_csv_and_send_to_kafka(csv_file)
+    producer.flush()
